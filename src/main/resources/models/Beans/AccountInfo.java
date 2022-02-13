fb6b26abe4e4d5bcfb5e85bb0a9b9df1a8248a53
kaede.maruyama <maru39maru@icloud.com>
2022-02-13 21:32:13 +0900

    fix pom.xml

---
+++ src/main/resources/models/Beans/AccountInfo.java
@@ -0,0 +1,58 @@
+package main.resources.models.Beans;
+
+public class AccountInfo {
+
+    @SuppressWarnings("unused")
+    private static final long serialVersionUID = 1L;
+
+    private String firstName;
+    private String lastName;
+    private String birthDate;
+    private int birthDateNum;
+
+    public AccountInfo(String firstName, String lastName, String birthDate) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        try {
+        this.birthDateNum = Integer.parseInt(birthDate.replace("-", ""));
+        } catch(NumberFormatException e) {
+            this.birthDateNum = 00000000;
+        }
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public int getBirthDateNum() {
+        return birthDateNum;
+    }
+
+    public void setBirthDateNum(int birthDateNum) {
+        this.birthDateNum = birthDateNum;
+    }
+
+
+}
